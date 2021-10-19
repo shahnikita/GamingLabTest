505fd4ea754781ba2ebd95313c4e3bb082908eea
yashp <yashp@DESKTOP-OQGTFH1>
2021-10-19 16:32:36 -0300

    Add project files.

---
+++ GamingLabTest/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingLabTest
+{
+    public class Game
+    {
+        /// <summary>
+        /// the primary key of the Game.
+        /// </summary>
+        public int GameID { get; set; }
+
+        /// <summary>
+        /// the name of the Game.
+        /// </summary>
+        public string GameName { get; set; }
+        /// <summary>
+        /// the date that the Game was submitted to GLI by the customer.
+        /// </summary>
+        public DateTime ReceivedDate { get; set; }
+        /// <summary>
+        /// the date that the Game was accepted for testing by a GLI Engineering Manager.
+        /// </summary>
+        public DateTime AcceptedDate { get; set; }
+        /// <summary>
+        /// the total dollar amount that GLI stands to gain by testing the Game.
+        /// </summary>
+        public decimal ContractValue { get; set; }
+        /// <summary>
+        /// the level of complexity that the Game represents (Low, Medium, or High)
+        /// </summary>
+        public GameComplexity Complexity { get; set; }
+    }
+
+    public enum GameComplexity
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public class GameList
+    {
+        public List<Game> Games { get; set; }
+    }
+}
---
+++ GamingLabTest/Program.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GamingLabTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = LoadGames();
+            Requirement1(games);
+            Requirement2(games);
+            Requirement3(games);
+
+
+        }
+
+        public static GameList LoadGames()
+        {
+            try
+            {
+                var gameList = new GameList();
+                var filePath = "games.json";
+                using (var r = new StreamReader(filePath))
+                {
+                    var json = r.ReadToEnd();
+                    gameList = JsonConvert.DeserializeObject<GameList>(json);
+                }
+
+                return gameList;
+            }
+            catch (FileNotFoundException notfound)
+            {
+                Console.WriteLine(notfound.FileName + "not found");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public static Game Requirement1(GameList games)
+        {
+            var game = new Game();
+
+            game = games.Games.OrderByDescending(x => x.ContractValue)
+                .ThenBy(x => x.ReceivedDate)
+                .ThenBy(x => x.Complexity)
+                .ThenBy(x => (x.AcceptedDate - x.ReceivedDate).Days).FirstOrDefault();
+
+            return game;
+        }
+
+        public static List<KeyValuePair<string, double>> Requirement2(GameList games)
+        {
+
+            List<KeyValuePair<string, double>> avgList = games.Games
+                .GroupBy(x => x.Complexity)
+                .Select(y => KeyValuePair.Create(y.Key.ToString(), y.Average(u => NumberOfWorkhrs(u.ReceivedDate, u.AcceptedDate)))).ToList();
+
+            return avgList;
+        }
+
+        private static int NumberOfWorkhrs(DateTime start, DateTime end)
+        {
+            var workDays = 0;
+            while (start != end)
+            {
+                if (start.DayOfWeek != DayOfWeek.Saturday && start.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    workDays++;
+                }
+
+                start = start.AddDays(1);
+            }
+
+            return workDays * 8;
+        }
+
+        public static List<Game> Requirement3(GameList games)
+        {
+            var totalAmt = 30000;
+            var contractValue = 0;
+
+            games.Games = games.Games.OrderBy(x => x.Complexity).ThenByDescending(x => x.ContractValue).ToList();
+            var returnList = new List<Game>();
+
+
+            foreach (var game in games.Games)
+            {
+                contractValue = (int)(contractValue + game.ContractValue);
+                returnList.Add(game);
+                if (contractValue >= totalAmt)
+                {
+                    break;
+                }
+            }
+
+            return returnList;
+        }
+
+    }
+}
